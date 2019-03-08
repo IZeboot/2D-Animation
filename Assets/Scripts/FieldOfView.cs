b4a1bcda2221ffcc0a0fb2b35076a2f26a9f97ae
Cong PV <>
2019-03-08 17:32:31 +0700

    -11

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewAngle;
+    [Range(0,360)]
+    public float viewRadius;
+
+    public Vector3 DirFromAngle(float angleInDegrres, bool isGlobalAngle)
+    {
+        if (!isGlobalAngle)
+        {
+            angleInDegrres += transform.position.y;
+        }
+
+        return new Vector3(Mathf.Cos(angleInDegrres * Mathf.Deg2Rad), Mathf.Sin(angleInDegrres * Mathf.Deg2Rad) );
+    }
+
+}
